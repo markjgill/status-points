7d4bd28827e51a4bcb2e548c7d3d90b10fa447b2
Mark Gill <markgillnz@gmail.com>
2022-04-13 18:18:52 +1200

    Refactor the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,9 +9,9 @@ import { Button } from 'primereact/button';
 import { addStatusPointsRequest } from '../reducers/statusPoints';
 
 const Form = () => {
-    const [date, setDate] = useState(null);
-    const [type, setType] = useState(null);
-    const [points, setPoints] = useState(null);
+    const [date, setDate] = useState(undefined);
+    const [type, setType] = useState(undefined);
+    const [points, setPoints] = useState(undefined);
 
     const dispatch = useDispatch();
 
@@ -21,42 +21,32 @@ const Form = () => {
     };
 
     const clearForm = () => {
-        setDate(null);
-        setType(null);
-        setPoints(null);
+        setDate(undefined);
+        setType(undefined);
+        setPoints(undefined);
     };
 
     const types = ['Flight', 'Card'];
 
     return (
-        <div className="grid align-items-center">
-            <div className="p-fluid col-12 md:col-2">
-                <span className="p-float-label">
-                    <Calendar id="date" dateFormat="dd M yy" value={date} onChange={({ value }) => setDate(DateTime.fromJSDate(value))} />
-                    <label htmlFor="date">Date</label>
-                </span>
-            </div>
-            <div className="p-fluid col-12 md:col-2">
-                <span className="p-float-label">
-                    <InputNumber id="points" inputId="minmaxfraction" mode="decimal" minFractionDigits={0} maxFractionDigits={2} value={points} onValueChange={({ value }) => setPoints(value)} />
-                    <label htmlFor="points">Points</label>
-                </span>
-            </div>
-            <div className="p-fluid col-12 md:col-2">
+        <div className="formgroup-inline align-items-center">
+            <span className="field">
+                <Calendar id="calendar" dateFormat="dd M yy" value={date} placeholder="Date" onChange={({ value }) => setDate(DateTime.fromJSDate(value))} />
+            </span>
+            <span className="field p-float-label">
+                <InputNumber id="points" inputId="minmaxfraction" mode="decimal" minFractionDigits={0} maxFractionDigits={2} value={points} placeholder="Points" onValueChange={({ value }) => setPoints(value)} />
+            </span>
+            <div className="field">
                 <SelectButton options={types} value={type} onChange={({ value }) => setType(value)} />
             </div>
-            <div className="col-12 md:col-2">
-                <div className="grid">
-                    <div className="p-fluid col-6 md:col-4">
-                        <Button icon="pi pi-plus-circle" label="Add" onClick={addStatusPoints} />
-                    </div>
-                    <div className="p-fluid col-6 md:col-4">
-                        <Button icon="pi pi-times-circle" label="Clear" onClick={clearForm} />
-                    </div>
-                </div>
+            <div className="field">
+                <Button icon="pi pi-plus-circle" label="Add" onClick={addStatusPoints} />
+            </div>
+            <div className="field">
+                <Button icon="pi pi-times-circle" label="Clear" onClick={clearForm} />
             </div>
         </div>
-    )
+    );
 };
 
 export default Form;
