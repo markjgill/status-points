843ef51e67d8f523c9b77e5f984e6420d4d64906
Mark Gill <markgillnz@gmail.com>
2022-10-02 18:49:47 +1300

    Move data fetching to App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { DateTime, Interval } from 'luxon';
+import { always, cond, gte, T, __ } from 'ramda';
 
 import Header from './components/Header';
 import Contents from './components/Contents';
 import SettingsSidebar from './components/SettingsSidebar';
-import { fetchStatusPointsRequest } from './reducers/statusPoints';
+import { fetchStatusPointsRequest, setCurrentPoints, setCurrentTier } from './reducers/statusPoints';
 import { fetchSettingsRequest } from './reducers/settings';
 
 const App = () => {
   const dispatch = useDispatch();
+  const statusPoints = useSelector(state => state.statusPoints.statusPoints);
+  const { silver, gold, elite } = useSelector(state => state.settings.points);
+
+  const endOfToday = DateTime.now().endOf("day");
+  const lastTwelveMonths = Interval.fromDateTimes(endOfToday.minus({ months: 12 }), endOfToday);
+  const currentPoints = statusPoints.filter(({ date }) => lastTwelveMonths.contains(date))
+    .map(({ points }) => points)
+    .reduce((acc, val) => acc + val, 0);
+
+  const currentTier = cond([
+    [gte(__, elite), always("eiite")],
+    [gte(__, gold), always("gold")],
+    [gte(__, silver), always("silver")],
+    [T, always("none")]
+  ])(currentPoints);
+
   useEffect(() => {
     dispatch(fetchSettingsRequest());
     dispatch(fetchStatusPointsRequest());
-  });
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(setCurrentPoints(currentPoints));
+    dispatch(setCurrentTier(currentTier));
+  }, [dispatch, currentPoints, currentTier]);
 
   return (
     <>
--- src/components/PointsSummary.js
+++ src/components/PointsSummary.js
@@ -1,43 +1,25 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { DateTime, Interval } from 'luxon';
+import { useSelector } from 'react-redux';
 import { always, cond, gte, T, __ } from 'ramda';
 import { Card } from 'primereact/card';
 import { Knob } from 'primereact/knob';
 
-import { setCurrentPoints, setCurrentTier } from '../reducers/statusPoints';
-
 const PointsSummary = () => {
-    const dispatch = useDispatch();
-    const statusPoints = useSelector(state => state.statusPoints.statusPoints);
+    const currentPoints = useSelector(state => state.statusPoints.currentPoints);
     const { silver, gold, elite } = useSelector(state => state.settings.points);
 
-    const endOfToday = DateTime.now().endOf("day");
-    const lastTwelveMonths = Interval.fromDateTimes(endOfToday.minus({ months: 12 }), endOfToday);
-
-    const filteredStatusPoints = statusPoints.filter(({ date }) => lastTwelveMonths.contains(date));
-
-    const totalPoints = filteredStatusPoints.map(({ points }) => points)
-        .reduce((acc, val) => acc + val, 0);
-
-    const { currentTier, max } = cond([
-        [gte(__, elite), always({ currentTier: "eiite", max: Infinity })],
-        [gte(__, gold), always({ currentTier: "gold", max: elite })],
-        [gte(__, silver), always({ currentTier: "silver", max: gold })],
-        [T, always({ currentTier: "none", max: silver })]
-    ])(totalPoints);
-
-    useEffect(() => {
-        dispatch(setCurrentPoints(totalPoints));
-        dispatch(setCurrentTier(currentTier));
-    }, [dispatch, totalPoints, currentTier]);
+    const max = cond([
+        [gte(__, elite), always(Infinity)],
+        [gte(__, gold), always(elite)],
+        [gte(__, silver), always(gold)],
+        [T, always(silver)]
+    ])(currentPoints);
 
     return (
         <Card className="border-1">
             <div className="flex flex-column">
                 <h2 className="flex justify-content-center">Points Summary</h2>
                 <div className="flex justify-content-center">
-                    <Knob min={0} max={max} value={Math.trunc(totalPoints)} valueColor={"rgb(0, 255, 0)"} readOnly />
+                    <Knob min={0} max={max} value={Math.trunc(currentPoints)} valueColor={"rgb(0, 255, 0)"} readOnly />
                 </div>
             </div>
         </Card>
