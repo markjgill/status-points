ef12f04b7a8e33ba320e664eb3f17266905dc525
Mark Gill <markgillnz@gmail.com>
2022-10-30 17:56:05 +1300

    Uninstall redux-saga

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,9 @@
-import createSagaMiddleware from 'redux-saga'
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
 
-import authenticationReducer from '../reducers/authentication';
-import statusPointsReducer from '../reducers/statusPoints';
-import settingsReducer from '../reducers/settings';
+import authenticationReducer from "../reducers/authentication";
+import statusPointsReducer from "../reducers/statusPoints";
+import settingsReducer from "../reducers/settings";
 import userProfileReducer from "../reducers/userProfile";
-import rootSaga from '../sagas/rootSaga';
-
-const sagaMiddleware = createSagaMiddleware()
 
 const store = configureStore({
     reducer: {
@@ -15,10 +11,7 @@ const store = configureStore({
         statusPoints: statusPointsReducer,
         settings: settingsReducer,
         userProfile: userProfileReducer
-    },
-    middleware: [sagaMiddleware]
+    }
 });
-
-sagaMiddleware.run(rootSaga);
 
 export default store;
